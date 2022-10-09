dfb2518f72a373eec18a0cbd9b1a62ca148c0c00
Paweł Bytner <pawel.m.bytner@gmail.com>
2022-10-09 14:50:46 +0200

    Dodaj pliki projektów.

---
+++ ASP.NET 1.2 z1/Pages/Miasta.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP.NET_1._2_z1.Pages
+{
+    public class MiastaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ASP.NET 1.2 z1/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP.NET_1._2_z1.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
